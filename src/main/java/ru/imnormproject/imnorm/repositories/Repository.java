cb2880795fd8089d2e9caf44a1a2039746916d39
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-02-01 19:29:16 +0700

    Add abstract public methods into Repository.java

--- src/main/java/ru/imnormproject/imnorm/repositories/Repository.java
+++ src/main/java/ru/imnormproject/imnorm/repositories/Repository.java
@@ -9,18 +9,28 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public abstract class Repository {
-    protected final Set<Object> blockingRecord = new HashSet<>();
+public abstract class Repository<Value, Key> {
+    protected final Set<Value> blockingRecord = new HashSet<>();
     protected final Field recordId;
     protected final boolean needGenerateId;
 
-    protected Repository(Class type) {
+    protected Repository(Class<Value> type) {
         Field[] fields = Arrays.stream(type.getDeclaredFields())
                 .filter(field -> Objects.nonNull(field.getAnnotation(Id.class)))
                 .toArray(Field[]::new);
-        if(fields.length != 1)
+        if (fields.length != 1)
             throw new CountIdException(type);
         recordId = fields[0];
         needGenerateId = recordId.getAnnotation(Id.class).autoGenerate();
     }
+
+    public abstract Value save(Value o);
+
+    public abstract Value find(Key id);
+
+    public abstract Set<Value> findAll();
+
+    public abstract Set<Value> findAll(int startIndex, int rowCount);
+
+    public abstract Value delete(Key id);
 }
