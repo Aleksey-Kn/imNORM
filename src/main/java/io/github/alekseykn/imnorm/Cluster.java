843b7be67f8c968c08a67e0d3feca461ea0325db
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-02-02 12:35:05 +0700

    Create Cluster.java

---
+++ src/main/java/io/github/alekseykn/imnorm/Cluster.java
@@ -0,0 +1,43 @@
+package io.github.alekseykn.imnorm;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class Cluster<Value> {
+    private boolean redacted = false;
+    private final ConcurrentHashMap<Object, Value> data;
+
+    Cluster(ConcurrentHashMap<Object, Value> map) {
+        data = map;
+    }
+
+    void set(Object key, Value value) {
+        redacted = true;
+        data.put(key, value);
+    }
+
+    Value get(Object key) {
+        return data.get(key);
+    }
+
+    Collection<Value> findAll() {
+        return data.values();
+    }
+
+    Value delete(Object key) {
+        redacted = true;
+        return data.remove(key);
+    }
+
+    int size() {
+        return data.size();
+    }
+
+    boolean isRedacted() {
+        return redacted;
+    }
+
+    void wasFlush() {
+        redacted = false;
+    }
+}
