88ba4ef468dbe1bb7b16e2e8a26decb7283d9c4f
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-02-06 14:31:16 +0700

    Update Transaction.java

--- src/main/java/io/github/alekseykn/imnorm/Transaction.java
+++ src/main/java/io/github/alekseykn/imnorm/Transaction.java
@@ -3,17 +3,53 @@ package io.github.alekseykn.imnorm;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Transaction {
-    private final Map<Repository<?>, Map<String, Object>> blockedRecord = new HashMap<>();
+    private final static Set<Transaction> openTransactions = ConcurrentHashMap.newKeySet();
+
+    static {
+        Thread remover = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                Iterator<Transaction> iterator = openTransactions.iterator();
+                Transaction now;
+                while (iterator.hasNext()) {
+                    now = iterator.next();
+                    if(now.callingThreadIsDye()) {
+                        now.rollback();
+                        iterator.remove();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        remover.setDaemon(true);
+        remover.start();
+    }
+
+    private final Thread callingThread;
+    private Map<Repository<?>, Map<String, Object>> blockedRecord = new HashMap<>();
+
+    public Transaction() {
+        callingThread = Thread.currentThread();
+        openTransactions.add(this);
+    }
 
     protected void lock(Repository<?> provenance, String recordId, Object nowValue) {
+        if(Objects.isNull(blockedRecord))
+            throw new TransactionWasCloseException();
         if (!blockedRecord.containsKey(provenance)) {
             blockedRecord.put(provenance, new HashMap<>());
         }
         blockedRecord.get(provenance).put(recordId, nowValue);
     }
 
-    protected boolean recordWasLocked(Repository<?> provenance, String recordId) {
+    protected boolean recordWasLockedCurrentTransaction(Repository<?> provenance, String recordId) {
+        if(Objects.isNull(blockedRecord))
+            throw new TransactionWasCloseException();
         if(blockedRecord.containsKey(provenance)) {
             return blockedRecord.get(provenance).containsKey(recordId);
         } else return false;
@@ -21,9 +57,15 @@ public class Transaction {
 
     public void commit() {
         blockedRecord.forEach((repository, stringObjectMap) -> repository.unlock(stringObjectMap.keySet()));
+        blockedRecord = null;
     }
 
     public void rollback() {
         blockedRecord.forEach(Repository::rollback);
+        blockedRecord = null;
+    }
+
+    protected boolean callingThreadIsDye() {
+       return !callingThread.isAlive();
     }
 }
