db84f0cc25af54c75c683d1a8615a48da13d4012
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-06 16:03:13 +0700

    Added skipped synchronized in FastRepository

--- src/main/java/io/github/alekseykn/imnorm/FastRepository.java
+++ src/main/java/io/github/alekseykn/imnorm/FastRepository.java
@@ -8,34 +8,54 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Repository with full unloading clusters in RAM
+ *
+ * @param <Record> Type of entity for this repository
+ * @author Aleksey-Kn
+ */
 public final class FastRepository<Record> extends Repository<Record> {
+    /**
+     * The search tree where clusters and their initial keys are mapped
+     */
     private final TreeMap<String, Cluster<Record>> data = new TreeMap<>();
 
-    FastRepository(Class<Record> type, File directory) {
+    /**
+     * Load clusters from file system to RAM
+     *
+     * @param type      Type of data entity
+     * @param directory The directory where the clusters are saved
+     */
+    FastRepository(final Class<Record> type, final File directory) {
         super(type, directory);
         Scanner scanner;
         Record now;
         TreeMap<String, Record> tempClusterData;
         try {
-            for (File file : Objects.requireNonNull(directory.listFiles())) {
-                if (!file.getName().equals("_sequence.imnorm")) {
-                    tempClusterData = new TreeMap<>();
-                    scanner = new Scanner(file);
-                    while (scanner.hasNextLine()) {
-                        now = gson.fromJson(scanner.nextLine(), type);
-                        tempClusterData.put(getIdFromRecord.apply(now), now);
-                    }
-                    data.put(file.getName(), new Cluster<>(tempClusterData, this));
-                    scanner.close();
+            for (File file : Objects.requireNonNull(directory.listFiles((dir, name) ->
+                    !name.equals("_sequence.imnorm")))) {
+                tempClusterData = new TreeMap<>();
+                scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    now = gson.fromJson(scanner.nextLine(), type);
+                    tempClusterData.put(getIdFromRecord.apply(now), now);
                 }
+                data.put(file.getName(), new Cluster<>(tempClusterData, this));
+                scanner.close();
             }
         } catch (FileNotFoundException e) {
             throw new InternalImnormException(e);
         }
     }
 
+    /**
+     * Find cluster, which can contains current id
+     *
+     * @param id Record id, for which execute search
+     * @return Cluster, which can contains current id, or null, if such cluster not contains in data storage
+     */
     @Override
-    protected synchronized Cluster<Record> findCurrentClusterFromId(String id) {
+    protected synchronized Cluster<Record> findCurrentClusterFromId(final String id) {
         Map.Entry<String, Cluster<Record>> entry = data.floorEntry(id);
         if (Objects.isNull(entry)) {
             return null;
@@ -44,8 +64,16 @@ public final class FastRepository<Record> extends Repository<Record> {
         }
     }
 
+    /**
+     * Add new record to repository. If not exist suitable cluster, a new cluster is being created.
+     * If cluster too large, split it on two cluster.
+     *
+     * @param id     String interpretation of id
+     * @param record The record being added to data storage
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    protected synchronized Record create(final String id, final Record record) {
+    protected synchronized void create(final String id, final Record record) {
         if (data.isEmpty() || data.firstKey().compareTo(id) > 0) {
             data.put(id, new Cluster<>(id, record, this));
         } else {
@@ -57,11 +85,18 @@ public final class FastRepository<Record> extends Repository<Record> {
                 data.put(currentCluster.getFirstKey(), newCluster);
             }
         }
-        return record;
     }
 
+    /**
+     * Add new record to repository. If not exist suitable cluster, a new cluster is being created.
+     *
+     * @param id          String interpretation of id
+     * @param record      The record being added to data storage
+     * @param transaction Transaction, in which execute create
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    protected synchronized Record create(final String id, final Record record, final Transaction transaction) {
+    protected synchronized void create(final String id, final Record record, final Transaction transaction) {
         if (data.isEmpty() || data.firstKey().compareTo(id) > 0) {
             data.put(id, new Cluster<>(id, record, this, transaction));
         } else {
@@ -69,9 +104,14 @@ public final class FastRepository<Record> extends Repository<Record> {
             assert currentCluster != null;
             currentCluster.set(id, record, transaction);
         }
-        return record;
     }
 
+    /**
+     * Find all data, contains in this repository
+     *
+     * @return All record, contains in this repository
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
     public synchronized Set<Record> findAll() {
         return data.values().stream()
@@ -79,14 +119,29 @@ public final class FastRepository<Record> extends Repository<Record> {
                 .collect(Collectors.toSet());
     }
 
+    /**
+     * Find all data, contains in this repository with current transaction
+     *
+     * @param transaction Transaction, in which execute find
+     * @return All record, contains in current transaction
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public synchronized Set<Record> findAll(Transaction transaction) {
+    public synchronized Set<Record> findAll(final Transaction transaction) {
         return data.values().stream()
                 .flatMap(recordCluster -> recordCluster.findAll(transaction).stream())
                 .collect(Collectors.toSet());
     }
 
-    private Set<Record> pagination(List<Collection<Record>> clustersData, int startIndex, int rowCount) {
+    /**
+     * Pagination data for findAll method
+     *
+     * @param clustersData Data from all clusters this repository
+     * @param startIndex   Quantity skipped records from start collection
+     * @param rowCount     Record quantity, which need return
+     * @return Part of the repository data obtained based on pagination conditions
+     */
+    private Set<Record> pagination(final List<Collection<Record>> clustersData, int startIndex, int rowCount) {
         List<Record> afterSkippedClusterValues;
         HashSet<Record> result = new HashSet<>(rowCount);
         for (Collection<Record> clusterRecord : clustersData) {
@@ -109,8 +164,16 @@ public final class FastRepository<Record> extends Repository<Record> {
         return result;
     }
 
+    /**
+     * Find all records with pagination in this repository
+     *
+     * @param startIndex Quantity skipped records from start collection
+     * @param rowCount   Record quantity, which need return
+     * @return All record, contains in current diapason
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public Set<Record> findAll(int startIndex, int rowCount) {
+    public Set<Record> findAll(final int startIndex, final int rowCount) {
         List<Collection<Record>> clustersData;
         synchronized (this) {
             clustersData = data.values().stream()
@@ -120,8 +183,17 @@ public final class FastRepository<Record> extends Repository<Record> {
         return pagination(clustersData, startIndex, rowCount);
     }
 
+    /**
+     * Find all records with pagination in current repository with current transaction
+     *
+     * @param startIndex  Quantity skipped records from start collection
+     * @param rowCount    Record quantity, which need return
+     * @param transaction Transaction, in which execute find
+     * @return All record, contains in current transaction in current diapason
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public Set<Record> findAll(int startIndex, int rowCount, Transaction transaction) {
+    public Set<Record> findAll(final int startIndex, final int rowCount, final Transaction transaction) {
         List<Collection<Record>> clustersData;
         synchronized (this) {
             clustersData = data.values().stream()
@@ -131,8 +203,15 @@ public final class FastRepository<Record> extends Repository<Record> {
         return pagination(clustersData, startIndex, rowCount);
     }
 
+    /**
+     * Remove record with current id. If current cluster becomes empty it is deleted.
+     *
+     * @param id Id of the record being deleted
+     * @return Record, which was deleted from repository, or null, if specified record not exist
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public synchronized Record deleteById(Object id) {
+    public synchronized Record deleteById(final Object id) {
         String realId = String.valueOf(id);
         Cluster<Record> cluster = findCurrentClusterFromId(realId);
         if (Objects.isNull(cluster)) {
@@ -150,8 +229,17 @@ public final class FastRepository<Record> extends Repository<Record> {
         return record;
     }
 
+    /**
+     * Remove record with current id in current transaction
+     *
+     * @param id          Id of the record being deleted
+     * @param transaction Transaction, in which execute delete
+     * @return Record, which was deleted from repository in current transaction, or null,
+     * if specified record not exist in current transaction
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public Record deleteById(Object id, Transaction transaction) {
+    public synchronized Record deleteById(final Object id, final Transaction transaction) {
         String realId = String.valueOf(id);
         Cluster<Record> cluster = findCurrentClusterFromId(realId);
         if (Objects.isNull(cluster)) {
@@ -160,12 +248,20 @@ public final class FastRepository<Record> extends Repository<Record> {
         return cluster.delete(realId, transaction);
     }
 
+    /**
+     * Clear current repository from file system and RAM
+     *
+     * @throws DeadLockException Current record lock from other transaction
+     */
     @Override
-    public void deleteAll() {
+    public synchronized void deleteAll() {
         super.deleteAll();
         data.clear();
     }
 
+    /**
+     * Save data from current repository to file system
+     */
     @Override
     public synchronized void flush() {
         if (needGenerateId) {
@@ -179,4 +275,3 @@ public final class FastRepository<Record> extends Repository<Record> {
         data.forEach((id, cluster) -> cluster.flush(new File(directory.getAbsolutePath(), String.valueOf(id)), gson));
     }
 }
-
