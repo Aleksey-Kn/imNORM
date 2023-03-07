0c6e189c7c98685bbbc0990616fb5d0c894ebe58
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-07 15:18:37 +0700

    Change throws from Transaction.blockingTransaction exception

--- src/main/java/io/github/alekseykn/imnorm/Transaction.java
+++ src/main/java/io/github/alekseykn/imnorm/Transaction.java
@@ -19,6 +19,7 @@ import java.util.function.Consumer;
  * Using only a blocking transaction ensures that no exception is thrown.
  * Waiting transactions will try to run in parallel. When accessing a cluster captured by another transaction,
  * the transaction will wait for it to be unlocked and, if the waiting time is too long, it will throw an exception.
+ * If the thread from which the transaction was created is interrupted, the transaction is canceled automatically.
  *
  * @author Aleksey-Kn
  */
@@ -54,14 +55,41 @@ public class Transaction {
         remover.start();
     }
 
+    /**
+     * Open waiting transaction with specified time of wait.
+     * When in such transaction accesses a blocked cluster,
+     * it waits for the resource to be released for the specified time.
+     * If this time is exceeded, a DeadLockException is thrown.
+     *
+     * @param waitBeforeThrowException Max wait time release cluster
+     * @return New transaction
+     */
     public static Transaction waitingTransaction(final int waitBeforeThrowException) {
         return new Transaction(waitBeforeThrowException);
     }
 
+    /**
+     * Open waiting transaction with 250 ms of wait.
+     * When in such transaction accesses a blocked cluster,
+     * it waits for the resource to be released for the 250 ms.
+     * If this time is exceeded, a DeadLockException is thrown.
+     *
+     * @return New transaction
+     */
     public static Transaction waitingTransaction() {
         return new Transaction(250);
     }
 
+    /**
+     * Waits for the completion of all other transactions, and then creates a new one.
+     * Thus, it is guaranteed that when using only blocking transactions,
+     * the possibility of throwing an DeadLockException is excluded.
+     * However, the use of blocking transactions does not allow multiple transactions to work simultaneously,
+     * which negatively affects performance.
+     *
+     * @return New transaction
+     * @throws InternalImnormException The thread was abandoned while waiting for a new transaction to be created
+     */
     public static Transaction blockingTransaction() {
         synchronized (mutex) {
             try {
@@ -69,16 +97,37 @@ public class Transaction {
                     mutex.wait();
                 }
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new InternalImnormException(e);
             }
             return new Transaction(250);
         }
     }
 
+    /**
+     * Execute current procedure, automatically create, commit or rollback waiting transaction.
+     * If repository throw DeadLockException, rollback transaction and procedure retry.
+     * If throw other exception, rollback transaction and return this exception.
+     *
+     * @param transactionalCall Procedure to be executed.
+     *                          May be executed an unlimited number of times until an exception that is not a DeadLockException is thrown,
+     *                          or the transaction completes successfully.
+     * @return Exception, if procedure throw exception. Optional.empty() if procedure completed correctly.
+     */
     public static Optional<Exception> executeInWaitingTransactionWithReply(final Consumer<Transaction> transactionalCall) {
         return executeInWaitingTransactionWithReply(transactionalCall, 250);
     }
 
+    /**
+     * Execute current procedure, automatically create, commit or rollback waiting transaction.
+     * If repository throw DeadLockException, rollback transaction and procedure retry.
+     * If throw other exception, rollback transaction and return this exception.
+     *
+     * @param transactionalCall        Procedure to be executed.
+     *                                 May be executed an unlimited number of times until an exception that is not a DeadLockException is thrown,
+     *                                 or the transaction completes successfully.
+     * @param waitBeforeThrowException Max time to wait for the resource to be released
+     * @return Exception, if procedure throw exception. Optional.empty() if procedure completed correctly.
+     */
     public static Optional<Exception> executeInWaitingTransactionWithReply(final Consumer<Transaction> transactionalCall,
                                                                            final int waitBeforeThrowException) {
         Transaction transaction;
@@ -97,29 +146,63 @@ public class Transaction {
     }
 
 
+    /**
+     * Thread, from which was created this transaction
+     */
     private final Thread callingThread;
+
+    /**
+     * Clusters owned by this transaction
+     */
     private Set<Cluster<?>> blockingClusters = new HashSet<>();
+
+    /**
+     * The waiting time for the cluster to be released, if exceeded, it will be thrown DeadLockException
+     */
     @Getter(value = AccessLevel.PACKAGE)
     private final int waitTime;
 
+    /**
+     * @param waitBeforeThrow The waiting time for the cluster to be released,
+     *                        if exceeded, it will be thrown DeadLockException
+     */
     private Transaction(final int waitBeforeThrow) {
         callingThread = Thread.currentThread();
         openTransactions.add(this);
         waitTime = waitBeforeThrow;
     }
 
-    void captureLock(Cluster<?> provenance) {
+    /**
+     * Marks the current cluster as blocked by this transaction.
+     * Other transactions will not be able to access this cluster until the current transaction is completed.
+     *
+     * @param cluster Cluster, subject to blocking
+     * @throws TransactionWasClosedException Accessing a transaction after it is closed
+     */
+    void captureLock(final Cluster<?> cluster) {
         if (Objects.isNull(blockingClusters))
             throw new TransactionWasClosedException();
-        blockingClusters.add(provenance);
+        blockingClusters.add(cluster);
     }
 
-    boolean lockOwner(Cluster<?> provenance) {
+    /**
+     * Checks the current cluster as blocked by this transaction
+     *
+     * @param cluster The cluster being checked
+     * @return True, if this transaction own current cluster
+     * @throws TransactionWasClosedException Accessing a transaction after it is closed
+     */
+    boolean lockOwner(final Cluster<?> cluster) {
         if (Objects.isNull(blockingClusters))
             throw new TransactionWasClosedException();
-        return blockingClusters.contains(provenance);
+        return blockingClusters.contains(cluster);
     }
 
+    /**
+     * Save all changes, made in this transaction
+     *
+     * @throws TransactionWasClosedException Accessing a transaction after it is closed
+     */
     public void commit() {
         if (Objects.isNull(blockingClusters))
             throw new TransactionWasClosedException();
@@ -131,6 +214,11 @@ public class Transaction {
         }
     }
 
+    /**
+     * Cancel all changes, made in this transaction
+     *
+     * @throws TransactionWasClosedException Accessing a transaction after it is closed
+     */
     public void rollback() {
         if (Objects.isNull(blockingClusters))
             throw new TransactionWasClosedException();
@@ -142,7 +230,13 @@ public class Transaction {
         }
     }
 
+    /**
+     * Checking for the existence of the thread from which the current transaction was created
+     *
+     * @return True, if the thread from which the current transaction was created is dead
+     */
     private boolean callingThreadIsDye() {
         return !callingThread.isAlive();
     }
 }
+
