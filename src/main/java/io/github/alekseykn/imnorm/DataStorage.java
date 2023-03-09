423e81ed7410bcf60084aa30e36b8775f0ab50ac
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-09 13:31:18 +0700

    Added strictly repository getter

--- src/main/java/io/github/alekseykn/imnorm/DataStorage.java
+++ src/main/java/io/github/alekseykn/imnorm/DataStorage.java
@@ -23,7 +23,7 @@ public class DataStorage {
      * Create instances of DataStorage with standard path
      * @return Instances of DataStorage with standard path
      */
-    public static DataStorage getDataStorage() {
+    public synchronized static DataStorage getDataStorage() {
         return getDataStorage(Path.of("data"));
     }
 
@@ -32,7 +32,7 @@ public class DataStorage {
      * @param path The path where the data storage will be located
      * @return Instances of DataStorage with specified path
      */
-    public static DataStorage getDataStorage(Path path) {
+    public synchronized static DataStorage getDataStorage(Path path) {
         path = path.toAbsolutePath();
         File rootDataStorageDirectory = path.toFile();
         if (!rootDataStorageDirectory.exists()) {
@@ -61,12 +61,13 @@ public class DataStorage {
     }
 
     /**
-     * Create fast repository or return exists, if it was created earlier
+     * Create fast repository or return exists, if it was created earlier.
+     * If exists repository have other type, return repository other type instead of the requested.
      * @param clas Class of entity
      * @param <Value> Type of entity
      * @return Repository for work with current entity
      */
-    public <Value> Repository<Value> getPreferablyFastRepositoryForClass(Class<Value> clas) {
+    public synchronized <Value> Repository<Value> getPreferablyFastRepositoryForClass(Class<Value> clas) {
         if (!createdRepository.containsKey(clas)) {
             createdRepository.put(clas, new FastRepository<>(clas, directoryForRepository(clas)));
         }
@@ -74,17 +75,68 @@ public class DataStorage {
     }
 
     /**
-     * Create frugal repository or return exists, if it was created earlier
+     * Create frugal repository or return exists, if it was created earlier.
+     * If exists repository have other type, return repository other type instead of the requested
      * @param clas Class of entity
      * @param <Value> Type of entity
      * @return Repository for work with current entity
      */
-    public <Value> Repository<Value> getPreferablyFrugalRepositoryForClass(Class<Value> clas, int repositoryMaxMegabyteSize) {
+    public synchronized <Value> Repository<Value> getPreferablyFrugalRepositoryForClass(Class<Value> clas, int repositoryMaxMegabyteSize) {
         if (!createdRepository.containsKey(clas)) {
             createdRepository.put(clas, new FrugalRepository<>(clas, directoryForRepository(clas),
                     repositoryMaxMegabyteSize * 100));
         }
         return (Repository<Value>) createdRepository.get(clas);
+    }
+    
+    /**
+     * Create fast repository or return exists, if it was created earlier.
+     * If exists repository have other type,
+     * makes an existing repository unavailable for writing and creates a new repository of the specified type
+     *
+     * @param clas    Class of entity
+     * @param <Value> Type of entity
+     * @return Repository for work with current entity
+     */
+    public synchronized <Value> Repository<Value> getStrictlyFastRepositoryForClass(Class<Value> clas) {
+        if (createdRepository.containsKey(clas)) {
+            if(createdRepository.get(clas) instanceof FastRepository) {
+                return (Repository<Value>) createdRepository.get(clas);
+            } else {
+                Repository<?> oldRepository = createdRepository.get(clas);
+                oldRepository.flush();
+                oldRepository.lock();
+            }
+        }
+        Repository<Value> repository = new FastRepository<>(clas, directoryForRepository(clas));
+        createdRepository.put(clas, repository);
+        return repository;
+    }
+
+    /**
+     * Create frugal repository or return exists, if it was created earlier.
+     * If exists repository have other type,
+     * makes an existing repository unavailable for writing and creates a new repository of the specified type
+     *
+     * @param clas    Class of entity
+     * @param <Value> Type of entity
+     * @return Repository for work with current entity
+     */
+    public synchronized <Value> Repository<Value> getStrictlyFrugalRepositoryForClass(Class<Value> clas,
+                                                                         int repositoryMaxMegabyteSize) {
+        if (createdRepository.containsKey(clas)) {
+            if(createdRepository.get(clas) instanceof FrugalRepository) {
+                return (Repository<Value>) createdRepository.get(clas);
+            } else {
+                Repository<?> oldRepository = createdRepository.get(clas);
+                oldRepository.flush();
+                oldRepository.lock();
+            }
+        }
+        Repository<Value> repository = new FrugalRepository<>(clas, directoryForRepository(clas),
+                repositoryMaxMegabyteSize * 100);
+        createdRepository.put(clas, repository);
+        return repository;
     }
 
     /**
@@ -94,7 +146,7 @@ public class DataStorage {
      * @param <Value> Type of entity
      * @return Repository for work with current entity
      */
-    public <Value> Repository<Value> getRepositoryForClass(Class<Value> clas) {
+    public synchronized <Value> Repository<Value> getRepositoryForClass(Class<Value> clas) {
         if (!createdRepository.containsKey(clas)) {
             File repositoryDirectory = directoryForRepository(clas);
             if (repositoryDirectory.exists()) {
