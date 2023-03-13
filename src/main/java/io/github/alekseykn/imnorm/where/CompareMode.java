86ddf7ec9fd4586f6585da33c822e8c79cbf1b57
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-13 15:41:09 +0700

    Create CompareMode.java

---
+++ src/main/java/io/github/alekseykn/imnorm/where/CompareMode.java
@@ -0,0 +1,22 @@
+package io.github.alekseykn.imnorm.where;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public enum CompareMode {
+    EQUALS(cr -> cr == 0), NOT_EQUALS(cr -> cr != 0), MORE(cr -> cr > 0), LESS(cr -> cr < 0);
+
+    private final Predicate<Integer> condition;
+
+    CompareMode(final Predicate<Integer> cond) {
+        condition = cond;
+    }
+
+    <T> boolean checkCondition(Comparable<T> first, T second) {
+        return condition.test(first.compareTo(second));
+    }
+
+    <T> boolean checkCondition(T first, T second, Comparator<T> comparator) {
+        return condition.test(comparator.compare(first, second));
+    }
+}
