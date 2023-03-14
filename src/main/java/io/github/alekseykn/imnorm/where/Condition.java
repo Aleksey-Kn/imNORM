0eec052222d873977e975397914e8a3a0f4e1ee5
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-14 16:17:04 +0700

    Added FunctionalInterface annotaion into Condition

--- src/main/java/io/github/alekseykn/imnorm/where/Condition.java
+++ src/main/java/io/github/alekseykn/imnorm/where/Condition.java
@@ -6,9 +6,10 @@ package io.github.alekseykn.imnorm.where;
  * @param <E> Entity type
  * @author Aleksey-Kn
  */
+@FunctionalInterface
 public interface Condition<E> {
     /**
-     * Checks whether the passed object meets the condition
+     * Checks whether the passed record meets the condition
      *
      * @param record The record being checked
      * @return True, if the record fits the condition
