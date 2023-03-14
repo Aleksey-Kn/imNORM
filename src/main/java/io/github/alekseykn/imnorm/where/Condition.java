1052b5c26e1f0fa657b83656ecc83b8d2ec1b382
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-14 10:59:35 +0700

    Added javaDoc to Condition

--- src/main/java/io/github/alekseykn/imnorm/where/Condition.java
+++ src/main/java/io/github/alekseykn/imnorm/where/Condition.java
@@ -1,5 +1,13 @@
 package io.github.alekseykn.imnorm.where;
 
+/**
+ * Interface for checking the passage of the current object under the condition
+ */
 public interface Condition {
+    /**
+     * Checks whether the passed object meets the condition
+     * @param record The object being checked
+     * @return True, if the object fits the condition
+     */
     boolean fitsCondition(Object record);
 }
