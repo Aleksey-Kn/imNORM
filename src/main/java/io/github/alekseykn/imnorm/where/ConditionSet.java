e1a2d0fcf05c0464be03b028e9023241b1970d16
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-14 16:21:12 +0700

    Changed access modifiers of factory methods

--- src/main/java/io/github/alekseykn/imnorm/where/ConditionSet.java
+++ src/main/java/io/github/alekseykn/imnorm/where/ConditionSet.java
@@ -5,44 +5,99 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public final class ConditionSet implements Condition {
+/**
+ * A set of conditions for checking one or more fields of the same entity
+ *
+ * @param <E> Entity type
+ * @author Aleksey-Kn
+ */
+public final class ConditionSet<E> implements Condition<E> {
+    /**
+     * Multiple operations between conditions (true = 'and', false = 'or')
+     */
     private final List<Boolean> operationIsAnd;
-    private final List<Condition> conditions;
 
-    private ConditionSet(final boolean isAndOperation, final List<Condition> operands) {
+    /**
+     * All components of this ConditionSet
+     */
+    private final List<Condition<E>> conditions;
+
+    /**
+     * Create set of condition from input atomic conditions, united specified operator
+     *
+     * @param isAndOperation  Operation between conditions
+     * @param firstCondition  Condition to be added to the current set
+     * @param secondCondition Condition to be added to the current set
+     */
+    private ConditionSet(final boolean isAndOperation, final Condition<E> firstCondition,
+                         final Condition<E> secondCondition) {
         operationIsAnd = new LinkedList<>(List.of(isAndOperation));
-        conditions = new LinkedList<>(operands);
+        conditions = new LinkedList<>(List.of(firstCondition, secondCondition));
     }
 
-    static ConditionSet and(final Where<?> first, final Where<?> second) {
-        return new ConditionSet(true, List.of(first, second));
+    /**
+     * Create set of condition from input atomic conditions, united logic 'and'
+     *
+     * @param first Condition for create set of condition
+     * @param second Condition for create set of condition
+     * @param <E> Entity type
+     * @return Set of condition, united logic 'and'
+     */
+    public static <E> ConditionSet<E> and(final Condition<E> first, final Condition<E> second) {
+        return new ConditionSet<>(true, first, second);
     }
 
-    static ConditionSet or(final Where<?> first, final Where<?> second) {
-        return new ConditionSet(false, List.of(first, second));
+    /**
+     * Create set of condition from input atomic conditions, united logic 'or'
+     *
+     * @param first Condition for create set of condition
+     * @param second Condition for create set of condition
+     * @param <E> Entity type
+     * @return Set of condition, united logic 'or'
+     */
+    public static <E> ConditionSet<E> or(final Condition<E> first, final Condition<E> second) {
+        return new ConditionSet<>(false, first, second);
     }
 
 
-    public ConditionSet and(final Condition condition) {
+    /**
+     * Add specified condition through logic 'and' operator
+     *
+     * @param condition Condition to add
+     * @return Current ConditionSet
+     */
+    public ConditionSet<E> and(final Condition<E> condition) {
         operationIsAnd.add(true);
         conditions.add(condition);
         return this;
     }
-    
-    public ConditionSet or(final Condition condition) {
+
+    /**
+     * Add specified condition through logic 'or' operator
+     *
+     * @param condition Condition to add
+     * @return Current ConditionSet
+     */
+    public ConditionSet<E> or(final Condition<E> condition) {
         operationIsAnd.add(false);
         conditions.add(condition);
         return this;
     }
 
+    /**
+     * Checks whether the passed record meets the conditions, contain in this condition set
+     *
+     * @param record The record being checked
+     * @return True, if the record fits the condition
+     */
     @Override
-    public boolean fitsCondition(final Object record) {
+    public boolean fitsCondition(final E record) {
         List<Boolean> nowOperands = conditions.stream()
                 .map(condition -> condition.fitsCondition(record))
                 .collect(Collectors.toList());
         List<Boolean> nowOperations = new ArrayList<>(operationIsAnd);
-        for(int i = 0; i < nowOperations.size();) {
-            if(nowOperations.get(i)) {
+        for (int i = 0; i < nowOperations.size(); ) {
+            if (nowOperations.get(i)) {
                 nowOperands.set(i, nowOperands.get(i) && nowOperands.remove(i + 1));
                 nowOperations.remove(i);
             } else {
@@ -52,3 +107,4 @@ public final class ConditionSet implements Condition {
         return nowOperands.stream().anyMatch(b -> b);
     }
 }
+
