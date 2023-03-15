0dbbe5d8fbe5a4c311f5fea4df6458fd3e4d1fd4
Aleksey Kozhemyakin <47708567+Aleksey-Kn@users.noreply.github.com>
2023-03-15 17:30:54 +0700

    Renamed methods in FieldConditionTest

--- src/test/java/io/github/alekseykn/imnorm/where/FieldConditionTest.java
+++ src/test/java/io/github/alekseykn/imnorm/where/FieldConditionTest.java
@@ -10,67 +10,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FieldConditionTest {
 
     @Test
-    void andReturnTrue() {
+    void andShouldReturnTrue() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> id > 5)
                 .and(new FieldCondition<Integer, Dto>("id", id -> id < 10)).fitsCondition(new Dto(7)))
                 .isTrue();
     }
 
     @Test
-    void andReturnFalse() {
+    void andShouldReturnFalse() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> id > 5)
                 .and(new FieldCondition<Integer, Dto>("id", id -> id < 10)).fitsCondition(new Dto(12)))
                 .isFalse();
     }
 
     @Test
-    void orReturnTrue() {
+    void orShouldReturnTrue() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> id > 5)
                 .or(new FieldCondition<Integer, Dto>("id", id -> id < 10)).fitsCondition(new Dto(12)))
                 .isTrue();
     }
 
     @Test
-    void orReturnFalse() {
+    void orShouldReturnFalse() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> (id & 1) == 1)
                 .or(new FieldCondition<Integer, Dto>("id", id -> id < 10)).fitsCondition(new Dto(12)))
                 .isFalse();
     }
 
     @Test
-    void fitsConditionWithCompareModeMustReturnTrue() {
+    void fitsConditionWithCompareModeMustShouldReturnTrue() {
         assertThat(new FieldCondition<>("id", CompareMode.EQUALS, 10).fitsCondition(new Dto(10)))
                 .isTrue();
     }
 
     @Test
-    void fitsConditionWithCompareModeMustReturnFalse() {
+    void fitsConditionWithCompareModeMustShouldReturnFalse() {
         assertThat(new FieldCondition<>("id", CompareMode.MORE, 10).fitsCondition(new Dto(-1)))
                 .isFalse();
     }
 
     @Test
-    void fitsConditionWithComparatorMustReturnTrue() {
+    void fitsConditionWithComparatorMustShouldReturnTrue() {
         assertThat(new FieldCondition<>("id", CompareMode.NOT_EQUALS, 10,
                 Comparator.comparing(integer -> Integer.toString(integer).length())).fitsCondition(new Dto(100)))
                 .isTrue();
     }
 
     @Test
-    void fitsConditionWithComparatorModeMustReturnFalse() {
+    void fitsConditionWithComparatorModeMustShouldReturnFalse() {
         assertThat(new FieldCondition<>("id", CompareMode.LESS, 99,
                 Comparator.comparing(integer -> Integer.toString(integer).length())).fitsCondition(new Dto(-50)))
                 .isFalse();
     }
 
     @Test
-    void fitsConditionWithPredicateModeMustReturnTrue() {
+    void fitsConditionWithPredicateModeMustShouldReturnTrue() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> (id & 1) == 0).fitsCondition(new Dto(10)))
                 .isTrue();
     }
 
     @Test
-    void fitsConditionWithPredicateModeMustReturnFalse() {
+    void fitsConditionWithPredicateModeMustShouldReturnFalse() {
         assertThat(new FieldCondition<Integer, Dto>("id", id -> id % 2 == 1).fitsCondition(new Dto(10)))
                 .isFalse();
     }
